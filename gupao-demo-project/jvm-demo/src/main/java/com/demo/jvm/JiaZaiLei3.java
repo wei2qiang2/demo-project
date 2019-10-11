0fbde0fe2161d425410385da69f518d7d3e012a9
wq <wei.qiang@successinfo.com.cn>
2019-10-11 18:12:45 +0800

    update

---
+++ gupao-demo-project/jvm-demo/src/main/java/com/demo/jvm/JiaZaiLei3.java
@@ -0,0 +1,41 @@
+package com.demo.jvm;
+
+public class JiaZaiLei3 {
+
+   static class Hello{
+       /**
+        * 多线程同时创建对象时只执行一次
+        * Thread-0start
+        * Thread-0init ...
+        * Thread-1start
+        * Thread-1end
+        * Thread-0end
+        */
+       static {
+           System.out.println(Thread.currentThread().getName() + "init ...");
+           try {
+               Thread.sleep(3000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+   }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "start");
+                Hello jiaZaiLei3 = new Hello();
+                System.out.println(Thread.currentThread().getName() + "end");
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "start");
+                Hello jiaZaiLei3 = new Hello();
+                System.out.println(Thread.currentThread().getName() + "end");
+            }
+        }).start();
+    }
+}
---
+++ self-demo-project/se-demo-project/src/com/annotation/Anno04.java
@@ -0,0 +1,13 @@
+package com.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+//@Retention(RetentionPolicy.RUNTIME)
+//@Retention(RetentionPolicy.RUNTIME)
+
+@Documented
+public @interface Anno04 {
+    String className();
+    String methodName();
+}
